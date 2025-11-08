9625707198187ece4c788a3b6786482fd4964495
byunggwan <byunggwan827@gmail.com>
2025-11-08 10:11:18 +0900

    PROTO : 질문선택 후 DB저장까지

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AnswerModule } from './answer/answer.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
       inject: [ConfigService], // ConfigService 주입
     }),
+    AnswerModule,
 
   ],
 
